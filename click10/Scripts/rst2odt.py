2a8b072641c09bea611ae6af1ff151891215001c
Camilo Chaves <camilo.chaves.h@gmail.com>
2021-10-22 00:06:05 -0500

    carga de imagenes a la base de datos

---
+++ click10/Scripts/rst2odt.py
@@ -0,0 +1,30 @@
+#!C:\Users\camic\Documents\CS\MisionTIC2022\Ciclo 3\Programacion\Reto\Click10\Click10\Scripts\python.exe
+
+# $Id: rst2odt.py 5839 2009-01-07 19:09:28Z dkuhlman $
+# Author: Dave Kuhlman <dkuhlman@rexx.com>
+# Copyright: This module has been placed in the public domain.
+
+"""
+A front end to the Docutils Publisher, producing OpenOffice documents.
+"""
+
+import sys
+try:
+    import locale
+    locale.setlocale(locale.LC_ALL, '')
+except:
+    pass
+
+from docutils.core import publish_cmdline_to_binary, default_description
+from docutils.writers.odf_odt import Writer, Reader
+
+
+description = ('Generates OpenDocument/OpenOffice/ODF documents from '
+               'standalone reStructuredText sources.  ' + default_description)
+
+
+writer = Writer()
+reader = Reader()
+output = publish_cmdline_to_binary(reader=reader, writer=writer,
+    description=description)
+
